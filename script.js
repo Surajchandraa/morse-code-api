cc5c0c8974856cf666ac770036689b134f2af1f9
suraj <suraj@surajjosh7@gmail.com>
2024-03-13 21:59:56 +0530

    morse encode or decode feature added

--- script.js
+++ script.js
@@ -1,16 +1,26 @@
 const express = require('express');
-const morsedasta=require('./data')
-
-
+const morsecodedata=require('./morsedata');
+const textdata=require('./text_data');
+let pr=process.env.PORT || 5000;
 
 
 const app = express();
 
-const dataset=morsedasta;
+const dataset=textdata;
+const dataset2=morsecodedata
 
-let requireddata =(req,res,next)=>{
+let requireddata1 =(req,res,next)=>{
     if(!req.query.morsedata){
-        res.send("please enter a valid morse code");
+        res.status(400).send("please enter a valid morse code");
+    }
+    else{
+        next()
+    }
+}
+
+let requireddata2=(req,res,next)=>{
+    if(!req.query.textdata){
+        res.status(400).send("please enter a valid text");
     }
     else{
         next()
@@ -18,29 +28,43 @@ let requireddata =(req,res,next)=>{
 }
 
 
-app.get('/api/text-to-morse',requireddata, async(req,res)=>{
-    let morsecode=req.query.morsedata
-
-    let output= await calculation(morsecode,dataset)
-
-    output2=output.toString();
-    output3=output2.replace(/,/g,'')
-    console.log(output3)
-    res.send(output3)
+app.get('/morse-to-text', requireddata1, (req, res) => {
+    let morsecode = req.query.morsedata;
+    MorseCodeToText(morsecode, dataset, (err, data) => {
+        if (err) {
+            console.error("Error occurred:", err);
+            res.status(500).send("An error occurred.");
+        } else {
+            res.send(data);
+        }
+    });
+});
     
     
 
 
 
 
-})
 
-app.listen(5000,()=>{
-    console.log("http://localhost:5000/api/text-to-morse?morsedata=-... -.-.")
-})
 
-//... ..- .-. .- .---
-async function calculation(qry,dataa){
+app.get('/text-to-morse',requireddata2, (req, res) => {
+    let text = req.query.textdata;
+    TextToMorse(text, dataset2, (err, response) => {
+        if (err) {
+            console.error("Error occurred:", err);
+            res.status(500).send("An error occurred.");
+        } else {
+            res.send(response);
+        }
+    });
+});
+
+app.listen(pr, () => {
+    console.log(`Server is running on http://localhost:${pr}`);
+});
+
+
+ function MorseCodeToText(qry,dataa,callback){
     let queryy=qry.replace(/\s/g,',');
     let emptyarray=[];
 
@@ -48,10 +72,12 @@ async function calculation(qry,dataa){
     console.log(arrayofstring)
 
     let morseedata=dataa;
+    console.log(morseedata)
 
     for(let i=0;i<arrayofstring.length;i++){
         let morse_character = arrayofstring[i];
         let key = morseedata[morse_character];
+        console.log(key)
 
         if(key!=undefined){
             emptyarray.push(key)
@@ -63,6 +89,29 @@ async function calculation(qry,dataa){
             console.log("character not found for this morsecode");
         }
     }
-
-    return emptyarray;
+    
+    callback(null,emptyarray.join(''));
 }
+
+
+
+function TextToMorse(qry,dataa,callback){
+    let emptyarray=[];
+    let query=qry.split('');
+    
+    for(let i=0;i<query.length;i++){
+        let char=query[i];
+        let morse=dataa[char];
+
+        if(morse==undefined){
+            console.log("morse code not found for",char);
+        }
+        else{
+            emptyarray.push(morse);
+        }
+
+    }
+
+    callback(null,emptyarray.join(' '));
+}
+
